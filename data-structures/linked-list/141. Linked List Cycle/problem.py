dc03b52744fad2a908e5646f52c589cb3724c13d
elconnero <connerrobbins@csu.fullerton.edu>
2026-05-31 22:54:35 -0700

    We got this homie. We gonna push.

--- data-structures/linked-list/141. Linked List Cycle/problem.py
+++ data-structures/linked-list/141. Linked List Cycle/problem.py
@@ -6,19 +6,16 @@ class ListNode:
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
 
-        if head == None: return False
-        if head.next == None: return False
-
+        
         slow = head
-        fast = head.next.next
+        fast = head
 
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
 
-        while head:
-            if slow == fast: return True
-            else:
-                slow = slow.next
-                if fast.next.next == None: return False
-                else: fast = fast.next.next
+            if fast == slow: return True
+        return False
 
 node1 = ListNode(1)
 node2 = ListNode(2)
